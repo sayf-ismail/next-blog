2d502b72b2bc9af826faab99afd33a7efa9719ed
sayf-ismail <63899527+sayf-ismail@users.noreply.github.com>
2023-04-29 10:04:00 +0800

    Add ListItem, getFormattedDate lib function, dynamic route, remark

---
+++ app/posts/[postId]/page.tsx
@@ -0,0 +1,34 @@
+import { getSortedPostsData } from "@/lib/posts"
+import { notFound } from "next/navigation"
+
+export function generateMetaData({ params }: { params: { postId: string}}) {
+
+  const posts = getSortedPostsData() // deduped!
+  const { postId } = params
+
+  const post = posts.find(post => post.id === postId)
+
+  if (!post) {
+    return {
+      title: 'Post Not Found'
+    }
+  }
+
+  return {
+    title: post.title
+  }
+}
+
+export default async function Post({ params }: { params: { postId: string}}) {
+
+  const posts = getSortedPostsData() // deduped!
+  const { postId } = params
+
+  if (!posts.find(post => post.id === postId)) {
+    return notFound()
+  }
+
+  return (
+    <div>page</div>
+  )
+}
