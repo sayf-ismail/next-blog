747c76983179eeabd9a6e175e78514ab1941d8ea
sayf-ismail <63899527+sayf-ismail@users.noreply.github.com>
2023-04-29 09:44:18 +0800

    add posts, gray-matter, getSortedPostsData function

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
+import Posts from "./components/Posts";
 
 export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <main className="px-6 mx-auto">
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
-        Welcome Traveler 👋 &nbsp;
+        Welcome to my blog 👋 &nbsp;
       <span className="whitespace-nowrap">
         I&apos;m <span className="font-bold">Sayf</span>.
       </span>
       </p>
+      <Posts />
     </main>
   )
 }
