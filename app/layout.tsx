a81c6744c9a5c12fd7b85b4ce05e2ad19119c38f
sayf-ismail <63899527+sayf-ismail@users.noreply.github.com>
2023-04-29 09:02:51 +0800

    adjust page.tsx tailwind navbar text has maximum left

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from './components/Navbar'
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="dark:bg-slate-800">
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
